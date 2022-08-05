4e171f87595a7b37c361f0ba8de4fcf3716c6c0c
Markus Opolka <markus@martialblog.de>
2022-08-05 17:46:23 +0200

    add gofmt hook

--- lisp/init-golang.el
+++ lisp/init-golang.el
@@ -1,7 +1,9 @@
 (require-package 'go-mode)
 
 (add-hook 'go-mode-hook '(lambda ()
-  (setq tab-width 2
-  indent-tabs-mode t)))
+  (setq tab-width 2 indent-tabs-mode t)
+  (add-hook 'before-save-hook 'gofmt-before-save)
+  )
+)
 
 (provide 'init-golang)
