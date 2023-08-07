3194ff0f7a3ac42b758272bf50f2ae4b500b9035
Markus Opolka <markus@martialblog.de>
2023-08-07 10:30:40 +0200

    Fix syntax warning

--- lisp/init-golang.el
+++ lisp/init-golang.el
@@ -1,6 +1,6 @@
 (require-package 'go-mode)
 
-(add-hook 'go-mode-hook '(lambda ()
+(add-hook 'go-mode-hook #'(lambda ()
   (setq tab-width 2 indent-tabs-mode t)
   (add-hook 'before-save-hook 'gofmt-before-save)
   )
