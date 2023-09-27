52040e834b8f3f0f72cf676dae7820dee310d54c
Markus Opolka <markus@martialblog.de>
2023-09-27 15:09:51 +0200

    Configure PHP LSP

--- lisp/init-php-mode.el
+++ lisp/init-php-mode.el
@@ -1,6 +1,12 @@
 (require-package 'php-mode)
 
-;; TODO Configure LSP Server
-;; (add-hook 'php-mode-hook 'eglot-ensure)
+(defvar phpactor-executable (concat (getenv "HOME") "/github/php/phpactor/bin/phpactor"))
+
+(with-eval-after-load 'eglot
+ (add-to-list 'eglot-server-programs
+  `(php-mode . ("php" "-d" "memory_limit=512M" ,phpactor-executable "language-server")))
+)
+
+(add-hook 'php-mode-hook 'eglot-ensure)
 
 (provide 'init-php-mode)
